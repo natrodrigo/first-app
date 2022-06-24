9d9f0b7fe6d29efc4e25bee675cfbb0d2f14c281
Natã Rodrigo Santos Pereira <nata.rodrigo.p@gmail.com>
2022-06-24 17:01:07 -0300

    First app version returning a concat.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const cors = require('cors')
+const axios = require('axios');
+
+
 
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json()
@@ -10,20 +13,75 @@ const PORT = process.env.PORT || 5500;
 
 app.use(cors())
 
-app.listen(PORT,() => {
+app.listen(PORT, () => {
     console.log(`Server is running in port ${PORT}`)
 });
 
-app.get('/',(req,res) =>{
+app.get('/', (req, res) => {
     res.json({
-        msg:'Welcome to my API.'
+        msg: 'Welcome to my API.'
     })
 
 })
 
-app.post('/post',jsonParser,(req,res) =>{
-    let result = req.body.value*2
+app.post('/post', jsonParser, (req, res) => {
+    let uk = req.body.uk
+    let query = "https://public-api2.ploomes.com/"+ req.body.qry
+    const config = {
+        headers:{
+            "User-Key": req.body.uk
+        }
+      };
 
-    res.json({result})
 
-})
+    axios.get(query,config)
+        .then(response => {
+            let $ = response.data
+
+            $ = $.value
+
+            let opc = []
+            
+            for (item of $) {
+            
+                opc.push(item.ProductPart)
+            
+            }
+            
+            for (opcional of opc) {
+                delete opcional.Id
+                for (let [index,item] of opcional.OtherProperties.entries()) {
+                    delete item.Id
+                    delete item.FieldId
+                    delete item.FieldKey
+                    delete item.ProductId
+            
+                    Object.keys(item).forEach((key) => {
+                        if (item[key] == null) {
+                            //console.log(item)
+                            delete item[key]
+                        }
+                        else { opcional[`${index.toString()}${key}`] = item[key] }
+                    });
+            
+                }
+                delete opcional.OtherProperties
+            }
+            
+            let array = []
+            for ([index,opcional] of opc.entries()){
+            
+                Object.keys(opcional).forEach((key) => {
+            
+                    array[index] += `${opcional[key]}---`
+                    
+                });
+                array[index] = `${array[index].replace('undefined','')}***`
+            
+            }
+            let string = array.join('')
+            
+            res.json({"result" : string}) 
+        })
+
+});
